891ec3c0fd5c2ec14606b6ae43bcc8b03bc4f5cb
Matari73 <marianadeoliveira73@hotmail.com>
2024-04-20 15:18:57 -0300

    Estabelecida a comunicação com a api

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SelectOption from './SelectOption';
 import Input from './Input';
 
 function Search() {
   const [searchOption, setSearchOption] = useState('people'); 
-  const [searchText, setSearchText] = useState(''); 
+  const [searchText, setSearchText] = useState('');
+  const [searchUrl, setSearchUrl] = useState('');
 
   const handleSearchOptionChange = (option) => {
     setSearchOption(option);
@@ -16,9 +17,25 @@ function Search() {
     console.log(text)
   };
 
-  const handleSearch = () => {
+  useEffect(() => {
     const url = `https://swapi.dev/api/${searchOption}/?search=${searchText}`;
-    console.log('URL de busca:', url);
+    setSearchUrl(url);
+  }, [searchOption, searchText]);
+
+  const handleSearch = () => {
+    fetch(searchUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Falha ao buscar os dados');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Dados retornados:', data);
+        })
+        .catch(error => {
+            console.error('Erro ao buscar dados:', error);
+        });
   };
 
   return (
