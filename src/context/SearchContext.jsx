153b5f2eb560a5dd33bafeccf17bab220da97bd7
Matari73 <marianadeoliveira73@hotmail.com>
2024-04-28 16:15:02 -0300

    Corrigir inicialização de estado no componente Input

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -1,18 +1,19 @@
 import { createContext, useState } from "react";
 
-export const SearchContext = createContext()
+export const SearchContext = createContext();
 
 export const SearchProvider = ({ children }) => {
-    const [search, setSearch] = useState({
-        searchText: '',
-        option: 'people', // Defina a opção padrão aqui
-        searchResults: null,
-        error: false,
-    });
+  const [search, setSearch] = useState({
+    searchText: "",
+    option: "people",
+    searchResults: null,
+    error: false,
+    text: "",
+  });
 
-    return (
-        <SearchContext.Provider value={{ search, setSearch }}>
-            {children}
-        </SearchContext.Provider>
-    )
-}
+  return (
+    <SearchContext.Provider value={{ search, setSearch }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
